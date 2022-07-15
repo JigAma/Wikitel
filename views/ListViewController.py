30e9d86a7e95cb8b64c9710c6c58654e14784947
Nathan Duriez <NathanDuriez@protonmail.com>
2022-07-15 23:06:47 +0200

    REFACTOR de ListViewController

--- views/ListViewController.py
+++ views/ListViewController.py
@@ -5,31 +5,41 @@ from views.ViewController import *
 
 class ListViewController(ViewController):
 
-    def __init__(self, baudrate, port):
+    def __init__(self, baudrate, port, list_title, list_to_display=None):
         super().__init__(baudrate, port)
 
         self.top_of_page = ()  # x,y position of top of page
-        self.displayed_list = []  # current displayed list
+
+        self.list_title = list_title
+        self.displayed_list = []
+        if list_to_display:
+            self.displayed_list = list_to_display  # current displayed list
+            self.displayed_list = ["test", "salut", "coucou", "\n bonjou\nr", "\n", "a"]
 
     def draw(self):
         super().draw()
-
-        self._minitel.inverse(True)
-        self._minitel._print("Résultats de recherche pour:")
-        self._minitel.inverse(False)
-        self._minitel._print("\"Napoléon Ier, Roi de France et Empereur hihihihih\"\n")
+        #
+        # self._minitel.inverse(True)
+        # self._minitel._print("Résultats de recherche pour:")
+        # self._minitel.inverse(False)
+        self._minitel._print(self.list_title + "\n")
         self._minitel.plot("_", 40)
 
         self.top_of_page = self._minitel.curpos()
 
-    def print_list(self, list_to_print):
+        if self.displayed_list:
+            self.print_list()
+
+    def print_list(self, list_to_print=None):
         """
         Print a numerated list
 
         :param list[str] list_to_print: List of element to print
         """
-        self.displayed_list = list_to_print
-        for i, elem in enumerate(list_to_print):
+        if list_to_print:
+            self.displayed_list = list_to_print
+
+        for i, elem in enumerate(self.displayed_list):
             self._print_line(i+1, elem)
 
         self._minitel.pos(self._minitel.LINE_SIZE,
